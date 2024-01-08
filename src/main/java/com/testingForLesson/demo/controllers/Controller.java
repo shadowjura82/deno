23745bc9a7ba6caf885e2d500194e6587e7b7d62
shadowjura82 <shadowjura82@gmail.com>
2024-01-09 00:27:05 +0100

    Initial commit

---
+++ src/main/java/com/testingForLesson/demo/controllers/Controller.java
@@ -0,0 +1,12 @@
+package com.testingForLesson.demo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @GetMapping
+    public String getString() {
+        return "Software is working";
+    }
+}
